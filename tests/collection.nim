fc793cdad19102a22c43117be72d95bd9377583c
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-05-23 13:56:26 +0200

    Added more tests

---
+++ tests/collection.nim
@@ -0,0 +1,64 @@
+# Copyright 2017 UniCredit S.p.A.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest, nimat/dense
+
+
+suite "collection operations":
+  test "cumulative sum over 32-bit vectors":
+    let
+      v = @[1'f32, 3.5'f32, 2'f32, 4.5'f32]
+      w = cumsum(v)
+    check w == @[1'f32, 4.5'f32, 6.5'f32, 11'f32]
+  test "cumulative sum over 64-bit vectors":
+    let
+      v = @[1.0, 3.5, 2.0, 4.5]
+      w = cumsum(v)
+    check w == @[1.0, 4.5, 6.5, 11.0]
+  test "sum over 32-bit vectors":
+    let v = @[1'f32, 3.5'f32, 2'f32, 4.5'f32]
+    check v.sum == 11'f32
+  test "sum over 64-bit vectors":
+    let v = @[1.0, 3.5, 2.0, 4.5]
+    check v.sum == 11.0
+  test "mean over 32-bit vectors":
+    let v = @[1'f32, 3.5'f32, 2'f32, 4.5'f32]
+    check v.mean == 2.75'f32
+  test "mean over 64-bit vectors":
+    let v = @[1.0, 3.5, 2.0, 4.5]
+    check v.mean == 2.75
+  test "variance over 32-bit vectors":
+    let v = @[2'f32, 4'f32, 4'f32, 4'f32, 5'f32, 5'f32, 7'f32, 9'f32]
+    check v.variance == 4'f32
+  test "variance over 64-bit vectors":
+    let v = @[2.0, 4.0, 4.0, 4.0, 5.0, 5.0, 7.0, 9.0]
+    check v.variance == 4.0
+  test "standard deviation over 32-bit vectors":
+    let v = @[2'f32, 4'f32, 4'f32, 4'f32, 5'f32, 5'f32, 7'f32, 9'f32]
+    check v.stddev == 2'f32
+  test "standard deviation over 64-bit vectors":
+    let v = @[2.0, 4.0, 4.0, 4.0, 5.0, 5.0, 7.0, 9.0]
+    check v.stddev == 2.0
+  test "sum over 32-bit matrices":
+    let m = matrix(@[@[1'f32, 3.5'f32], @[2'f32, 4.5'f32]])
+    check m.sum == 11'f32
+  test "sum over 64-bit matrices":
+    let m = matrix(@[@[1.0, 3.5], @[2.0, 4.5]])
+    check m.sum == 11.0
+  test "mean over 32-bit matrices":
+    let m = matrix(@[@[1'f32, 3.5'f32], @[2'f32, 4.5'f32]])
+    check m.mean == 2.75'f32
+  test "mean over 64-bit matrices":
+    let m = matrix(@[@[1.0, 3.5], @[2.0, 4.5]])
+    check m.mean == 2.75
---
+++ tests/trivial_ops.nim
@@ -0,0 +1,140 @@
+# Copyright 2017 UniCredit S.p.A.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest, nimat/dense
+
+suite "trivial operations on 32-bit matrices":
+  test "reshape of matrices":
+    let
+      m1 = matrix(@[
+        @[1'f32, 0'f32, 2'f32, -1'f32],
+        @[-1'f32, 1'f32, 3'f32, 1'f32],
+        @[3'f32, 2'f32, 2'f32, 4'f32]
+      ])
+      m2 = matrix(@[
+        @[1'f32, 1'f32, 2'f32],
+        @[-1'f32, 2'f32, -1'f32],
+        @[3'f32, 2'f32, 1'f32],
+        @[0'f32, 3'f32, 4'f32]
+      ])
+    check m1.reshape(4, 3) == m2
+  test "turn vectors into matrices":
+    let
+      v = @[1'f32, -1'f32, 3'f32, 0'f32, 1'f32, 2'f32, 2'f32, 3'f32, 2'f32, -1'f32, 1'f32, 4'f32]
+      m = matrix(@[
+        @[1'f32, 0'f32, 2'f32, -1'f32],
+        @[-1'f32, 1'f32, 3'f32, 1'f32],
+        @[3'f32, 2'f32, 2'f32, 4'f32]
+      ])
+    check v.asMatrix(3, 4) == m
+  test "turn matrices into vectors":
+    let
+      v = @[1'f32, -1'f32, 3'f32, 0'f32, 1'f32, 2'f32, 2'f32, 3'f32, 2'f32, -1'f32, 1'f32, 4'f32]
+      m = matrix(@[
+        @[1'f32, 0'f32, 2'f32, -1'f32],
+        @[-1'f32, 1'f32, 3'f32, 1'f32],
+        @[3'f32, 2'f32, 2'f32, 4'f32]
+      ])
+    check m.asVector == v
+  test "transpose of matrices":
+    let
+      m1 = matrix(@[
+        @[1'f32, 0'f32, 2'f32, -1'f32],
+        @[-1'f32, 1'f32, 3'f32, 1'f32],
+        @[3'f32, 2'f32, 2'f32, 4'f32]
+      ])
+      m2 = matrix(@[
+        @[1'f32, -1'f32, 3'f32],
+        @[0'f32, 1'f32, 2'f32],
+        @[2'f32, 3'f32, 2'f32],
+        @[-1'f32, 1'f32, 4'f32]
+      ])
+    check m1.t == m2
+
+
+suite "trivial operations should share storage":
+  test "reshape of matrices":
+    var
+      m1 = matrix(@[
+        @[1.0, 0.0, 2.0, -1.0],
+        @[-1.0, 1.0, 3.0, 1.0],
+        @[3.0, 2.0, 2.0, 4.0]
+      ])
+      m2 = m1.reshape(4, 3)
+    m2[2, 1] = 0.0
+    check m1[0, 2] == 0.0
+  test "turn vectors into matrices":
+    var
+      v = @[1.0, -1.0, 3.0, 0.0, 1.0, 2.0, 2.0, 3.0, 2.0, -1.0, 1.0, 4.0]
+      m = v.asMatrix(3, 4)
+    m[2, 1] = 0.0
+    check v[5] == 0.0
+  test "turn matrices into vectors":
+    var
+      m = matrix(@[
+        @[1.0, 0.0, 2.0, -1.0],
+        @[-1.0, 1.0, 3.0, 1.0],
+        @[3.0, 2.0, 2.0, 4.0]
+      ])
+      v = m.asVector
+    v[5] = 0.0
+    check m[2, 1] == 0.0
+  test "transpose of matrices":
+    var
+      m1 = matrix(@[
+        @[1.0, 0.0, 2.0, -1.0],
+        @[-1.0, 1.0, 3.0, 1.0],
+        @[3.0, 2.0, 2.0, 4.0]
+      ])
+      m2 = m1.t
+    m2[1, 2] = 0.0
+    check m1[2, 1] == 0.0
+
+suite "trivial operations on 32-bit matrices should share storage":
+  test "reshape of matrices":
+    var
+      m1 = matrix(@[
+        @[1'f32, 0'f32, 2'f32, -1'f32],
+        @[-1'f32, 1'f32, 3'f32, 1'f32],
+        @[3'f32, 2'f32, 2'f32, 4'f32]
+      ])
+      m2 = m1.reshape(4, 3)
+    m2[2, 1] = 0'f32
+    check m1[0, 2] == 0'f32
+  test "turn vectors into matrices":
+    var
+      v = @[1'f32, -1'f32, 3'f32, 0'f32, 1'f32, 2'f32, 2'f32, 3'f32, 2'f32, -1'f32, 1'f32, 4'f32]
+      m = v.asMatrix(3, 4)
+    m[2, 1] = 0'f32
+    check v[5] == 0'f32
+  test "turn matrices into vectors":
+    var
+      m = matrix(@[
+        @[1'f32, 0'f32, 2'f32, -1'f32],
+        @[-1'f32, 1'f32, 3'f32, 1'f32],
+        @[3'f32, 2'f32, 2'f32, 4'f32]
+      ])
+      v = m.asVector
+    v[5] = 0'f32
+    check m[2, 1] == 0'f32
+  test "transpose of matrices":
+    var
+      m1 = matrix(@[
+        @[1'f32, 0'f32, 2'f32, -1'f32],
+        @[-1'f32, 1'f32, 3'f32, 1'f32],
+        @[3'f32, 2'f32, 2'f32, 4'f32]
+      ])
+      m2 = m1.t
+    m2[1, 2] = 0'f32
+    check m1[2, 1] == 0'f32
