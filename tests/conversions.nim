ff57ccf60b9f4e79a9c570572390beadcbcda995
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-05-23 12:19:03 +0200

    More tests

---
+++ tests/conversions.nim
@@ -0,0 +1,34 @@
+# Copyright 2017 UniCredit S.p.A.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest, nimat/dense
+
+
+suite "precision conversions":
+  test "vectors: 64 to 32 bits":
+    let v = @[1.0, 3.5, 2.0, 4.5]
+    check v.to32 == @[1'f32, 3.5'f32, 2'f32, 4.5'f32]
+  test "vectors: 32 to 64 bits":
+    let v = @[1'f32, 3.5'f32, 2'f32, 4.5'f32]
+    check v.to64 == @[1.0, 3.5, 2.0, 4.5]
+  test "matrices: 64 to 32 bits":
+    let
+      m = makeMatrix(3, 5, proc(i, j: int): float64 = (i + j).float64)
+      n = makeMatrix(3, 5, proc(i, j: int): float32 = (i + j).float32)
+    check m.to32 == n
+  test "matrices: 32 to 64 bits":
+    let
+      m = makeMatrix(3, 5, proc(i, j: int): float64 = (i + j).float64)
+      n = makeMatrix(3, 5, proc(i, j: int): float32 = (i + j).float32)
+    check n.to64 == m
