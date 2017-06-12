e73899661ca7398fc60434d8954f702540828070
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-06-12 08:44:55 +0200

    Added test

--- tests/dense/dslice.nim
+++ tests/dense/dslice.nim
@@ -164,6 +164,21 @@ suite "slicing column major matrices":
     ])
 
     check(m == expected)
+  test "mutable sum on slices":
+    var
+      m = makeMatrixIJ(float64, 5, 5, (3 * i + j).float64)
+      s = m[1 .. 3, 2 .. 4]
+      t = makeMatrixIJ(float64, 3, 3, (i - j).float64)
+    s += t
+    let expected = matrix(@[
+      @[0.0,  1.0,  2.0,  3.0,  4.0],
+      @[3.0,  4.0,  5.0,  5.0,  5.0],
+      @[6.0,  7.0,  9.0,  9.0,  9.0],
+      @[9.0,  10.0, 13.0, 13.0, 13.0],
+      @[12.0, 13.0, 14.0, 15.0, 16.0]
+    ])
+
+    check(m == expected)
 
 suite "slicing row major matrices":
   test "slice of a full matrix":
